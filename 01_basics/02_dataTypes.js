4f0dc4f38d4e87bd61107f7fcf5ac88bd1ad1e13
ansumanpatro11 <132783376+ansumanpatro11@users.noreply.github.com>
2024-12-05 19:12:45 +0000

    var and data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all JS Nodes as Newr version
+
+//alert(3+3) //we are using nodejs not browser
+
+console.log(typeof undefined); //undeefined
+console.log(typeof(null)); //object 
+
+let score="33"
+let valueinNumber=Number(score)
+console.log(typeof valueinNumber);
+
+//"33" => 3
+//"33abc"=>NaN
+
+
+// **************** operations *************
+
+let value=3
+let negValue=-value
---
+++ 01_basics/variable.js
@@ -0,0 +1,8 @@
+const acId="123";
+let accEmail="abc@gmail.com";
+
+{
+    let accEmail="acb";
+    console.log(accEmail);
+}
+console.table([acId,accEmail]);
